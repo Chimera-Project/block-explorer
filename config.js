399cc855f2c6119958741090fd54e9c5980d56d4
projectpixel1080 <49797418+projectpixel1080@users.noreply.github.com>
2019-08-18 18:34:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
-var api = 'http://45.79.23.218:18082';
+var api = 'http://185.105.4.162:43111';
 var donationAddress = "";
 var blockTargetInterval = 30; // enter the block interval in seconds
 var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  1920108000; // enter the total supply in atomic units
-var symbol = 'pxl'; // enter the coin's ticker
+var totalSupply =  570000000; // enter the total supply in atomic units
+var symbol = 'PXL'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
- "pxl": [
+ "PXL": [
 	[""]
  ]
 };
 
 var networkStat2 = {
-    "pxl": [
+    "PXL": [
 	[""]
  ]
 };
