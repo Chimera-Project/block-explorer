2439f16839d77781fd5cfd2f306cc9af53a7c91f
sudo-root <56373872+Chimera-Project@users.noreply.github.com>
2019-10-15 12:29:54 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 var api = 'http://185.105.4.162:43111';
-var donationAddress = "";
-var blockTargetInterval = 30; // enter the block interval in seconds
-var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  570000000; // enter the total supply in atomic units
-var symbol = 'PXL'; // enter the coin's ticker
-var refreshDelay = 30000;
-
-// pools stats by MainCoins
-var networkStat = {
- "PXL": [
-	[""]
- ]
-};
-
-var networkStat2 = {
-    "PXL": [
-	[""]
- ]
-};
+	var donationAddress = "";
+	var blockTargetInterval = 60; // enter the block interval in seconds
+	var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
+	var totalSupply =  100000000000; // enter the total supply in atomic units
+	var symbol = 'CMRA'; // enter the coin's ticker
+	var refreshDelay = 30000;
+	
+	// pools stats by MainCoins
+	var networkStat = {
+	 "CMRA": [
+		[""]
+	 ]
+	};
+	
+	var networkStat2 = {
+	    "CMRA": [
+		[""]
+	 ]
+	};
